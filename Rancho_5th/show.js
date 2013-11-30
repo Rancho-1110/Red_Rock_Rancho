7785dd225701f94b3ff013d78ada09e310743ee3
Rancho-1110 <ranco941110@gmail.com>
2013-11-30 13:42:24 +0800

    Red Rock 5

---
+++ Rancho_5th/show.js
@@ -0,0 +1,13 @@
+window.onload=function()
+{
+	var oShow=document.getElementById('nav_show');
+	var i;
+	for(i=0;i<oShow.child;i++)
+	{
+		if()
+		var oImg=oshow.childNodes;
+
+	}
+
+	alert(oImg.length)
+}
